63e5c4d642fa72ebb6beda1c76b04d883a8bdeff
U200709050 <emirhanpakyardim@gmail.com>
2023-01-05 12:32:14 +0300

    feat(socketio): implemented socket.io

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
+const http = require('http');
 const express = require('express');
 const path = require('path');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 const port = process.env.PORT || 3030;
 const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
-app.listen(port, () => {
+
+io.on('connection', (socket) => {
+    console.log("New web socket connection!")
+
+})
+
+server.listen(port, () => {
     console.log(`Server is up on port ${port}!`);
 })
