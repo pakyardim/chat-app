77b59f5a75b2c65de81c67d7fe46ffa06bf60ac7
U200709050 <emirhanpakyardim@gmail.com>
2023-01-05 13:21:01 +0300

    feat(chat): add simple imputs and socket connections to send/receive message

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,13 @@
-io()
+const socket = io();
+
+document.querySelector("#message-form").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const message = e.target.elements.message.value;
+
+  socket.emit("sendMessage", message);
+});
+
+socket.on("receiveMessage", (message) => {
+  console.log(message);
+});
--- src/index.js
+++ src/index.js
@@ -1,23 +1,27 @@
-const http = require('http');
-const express = require('express');
-const path = require('path');
-const socketio = require('socket.io');
+const http = require("http");
+const express = require("express");
+const path = require("path");
+const socketio = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
 const port = process.env.PORT || 3030;
-const publicDirectoryPath = path.join(__dirname, '../public');
+const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
 
+//emit sends an event while on listens an event.
 
-io.on('connection', (socket) => {
-    console.log("New web socket connection!")
+io.on("connection", (socket) => {
+  console.log("New web socket connection!");
 
-})
+  socket.on("sendMessage", (message) => {
+    io.emit("receiveMessage", message);
+  });
+});
 
 server.listen(port, () => {
-    console.log(`Server is up on port ${port}!`);
-})
+  console.log(`Server is up on port ${port}!`);
+});
